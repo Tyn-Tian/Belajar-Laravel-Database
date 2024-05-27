f3941198aae2cfd71df35c8b3f75106a57e3a384
Christian <kristian2015ok@gmail.com>
2024-05-27 23:26:09 +0700

    Add: Debug Query

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Log::listen(function (QueryExecuted $query) {
+            Log::info($query->sql);
+        });
     }
 }
